cc2f0a64b20cf60725842216e274cbcee8535f4a
55011212181Biiw <iMacATMSU>
2014-08-15 16:31:24 +0700

    เพิ่มๆๆๆ

--- swiftTutorial_1.playground/section-1.swift
+++ swiftTutorial_1.playground/section-1.swift
@@ -1,10 +1,23 @@
 let tutorialTeam = 56
 let editorialTeam = 23
-let totalTeam = tutorialTeam + editorialTeam
+var totalTeam = tutorialTeam + editorialTeam
 
 totalTeam += 1
-let priceInferred = -19
-let priceInExplicit:Int = -19
 
+let tutorialTeamInferred = 56
+let tutorialTeamExplicit: Int = 56
 
-1234
+let priceInferred = 19.99
+let priceExplicit:Double = 19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
