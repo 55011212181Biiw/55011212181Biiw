ba2790f740c25ecc75cfc70576f07b30162db3e5
55011212181Biiw <iMacATMSU>
2014-08-22 14:53:27 +0700

    Update Remove DIC

--- swiftTutorial_1.playground/section-1.swift
+++ swiftTutorial_1.playground/section-1.swift
@@ -37,7 +37,7 @@ shoppingList[0...3] = ["Bananas","Apples"]
 shoppingList
 
 
-//Add value to Array
+//Add value to Array**
 shoppingList += ["Baking Powder"]
 //shopping now content 4 items
 shoppingList += ["Chocolate spread","Cheese","Butter"]
@@ -61,7 +61,7 @@ shoppingList[4...6] = ["Bananas","Apples"]
 
 
 
-//Remove Values from Array
+//Remove Values from Array***
 let apples = shoppingList.removeLast()
 //the last item in the array has just been removed
 //shoppingList now contains 5 item,and no apples
@@ -71,6 +71,52 @@ let mapleSyrup = shoppingList.removeAtIndex(0)
 //the item that was at index 0 has just been removed
 //shoppingList now contains 6 item,and no Maple Syrup
 //the apples constant is now equal to the removed "Maple Syrup" string
+
+
+
+//Dictionary***
+var airports: [String: String] = ["TY0":"Tokyo","DUB":"dublin"]
+
+if airports.isEmpty{
+    println("The airports dictionary is empty.")
+
+}else{
+    println("The airports dictionary is not  empty.")
+}
+//prints "The airports dictionary is empty."
+println("The airports dictionary contains \(airports.count)item.")
+//prints "The airports dictionary contains  2 item."
+
+
+//Update and Add value Dictinary***
+
+airports["LHR"] = "London"
+//The airports dictionary now contains 3 items
+
+
+airports["LHR"] = "London Heathrow"
+//the value for "LHR"has been changed to "London Heathrow"
+
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB"){
+println("The old value for DUB was \(oldValue).")
+}
+//prints "The old value for DUB was Dublin."
+
+
+
+//Remove value Dictionary***
+airports["APL"] = "Apple International"
+//"Apple International" is not the real airport for APL,so delete it
+airports["APL"] = nil
+//APL has now been removed from the dictionary
+
+if let removedValue = airports.removeValueForKey("DUB"){
+    println("The removed airport's name is \(removedValue).")
+}else{
+    println("The airport dictionary does not contains a value for DUB.")
+}
+//prints "The removed airport's name is Dublin International."
+
 
 
 
