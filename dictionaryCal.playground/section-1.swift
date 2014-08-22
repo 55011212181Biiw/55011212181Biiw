0bb2b94412e25fdb3870956e13dae70906586ad5
55011212181Biiw <iMacATMSU>
2014-08-22 15:26:38 +0700

    แก้ไขเพิ่มเติม

--- dictionaryCal.playground/section-1.swift
+++ dictionaryCal.playground/section-1.swift
@@ -11,27 +11,27 @@ class TipCalculator {
         subtotal = total/(taxPct + 1)
     }
     //4
-    func calcTipwithTipPct(tipPct:Double)->Double{
+    func calcTipWithTipPct(tipPct:Double)->Double{
         return subtotal * tipPct
     }
     //1
     
-    func returnPossibleTips() -> [Int:Double]{
+    //1
+    func returnPossibleTips() ->[Int:Double]{
+        let possibleTipsInferred = [0.15,0.18,0.20]
+        let possibleTipsExplicit:[Double] = [0.15,0.18,0.20]
         
-        let possibleTipsInferred = [0.15,018,0.20]
-        let possibleTipExplicit:[Double] = [0.15,0.18,0.20]
-        var numberOfItems = possibleTipsInferred.count
         //2
-        var retval = [Int:Double]()
+        var retval = Dictionary<Int,Double>()
         for possibleTip in possibleTipsInferred{
-                let intPct = Int(possibleTip*100)
-                    //3
-                    retval[intPct] = calcTipwithTipPct(possibleTip)
+            let intPct = Int(possibleTip*100)
+            //3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
         }
         return retval
     }
     
 }
-        //6 
+//6
 let tipCalc = TipCalculator(total:33.25,taxPct:0.06)
 tipCalc.returnPossibleTips()
