8df12c62e410a4e85b062d59ba38f32bc74bef58
55011212181Biiw <iMacATMSU>
2014-08-22 13:52:18 +0700

    Array

--- swiftTutorial_1.playground/section-1.swift
+++ swiftTutorial_1.playground/section-1.swift
@@ -2,8 +2,8 @@ let tutorialTeam = 56
 let editorialTeam = 23
 var totalTeam = tutorialTeam + editorialTeam
 
-totalTeam += 1
 
+totalTeam += 1
 let tutorialTeamInferred = -19
 let tutorialTeamExplicit: Int = -19
 
@@ -24,3 +24,18 @@ if (onSaleInferred) {
 } else {
     println("\(nameInferred) at regular price: \(priceInferred)!")
 }
+
+//Array
+var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Egg","Milk"]
+//var shoppingListExplicit:[String] = ["Egg","Milk"]
+
+var shoppingList = ["Egg","Milk"]
+println("The shopping List contains \(shoppingList.count).items.")
+shoppingList.append("Flour")
+shoppingList += "Banking Powder"
+shoppingList += ["Chocolate Spread","cheese","Butter"]
+var firstItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+//shoppingList[0...3] = ["Bananas","Apples"]
+shoppingList
