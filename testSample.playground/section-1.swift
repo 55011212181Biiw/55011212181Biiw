a4ea987fbf05c52577a5eb94e359d92109597ee6
55011212181Biiw <iMacATMSU>
2014-09-03 14:51:12 +0700

    test0309

--- testSample.playground/section-1.swift
+++ testSample.playground/section-1.swift
@@ -1,2 +1,20 @@
-// ทำเอง
+
+
+//Matching 
+let  myScore = 65
+
+switch myScore {
+case 80...100:
+        println("grade A")
+case 70...80:
+        println("grade B")
+case 60...70:
+        println("grade C")
+case 50...60:
+        println("grade D")
+default:
+        println("Grade F")
+    
+}
+
 
