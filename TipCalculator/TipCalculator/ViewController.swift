c7f1a6d5779c7d64ec798f2b67c1d72e6e084bdd
55011212181Biiw <iMacATMSU>
2014-09-12 14:29:43 +0700

    เพิ่มเติม

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var totalTextField :UITextField!
+    @IBOutlet var taxPctSlider :UISlider!
+    @IBOutlet var taxPctLabel :UILabel!
+    @IBOutlet var resulTextView :UITextView!
+    
+    @IBAction func calculatorTapped(sender :AnyObject){}
+    @IBAction func taxPercentageChanged(sender :AnyObject){}
+    @IBAction func viewTapped(sender :AnyObject){}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
