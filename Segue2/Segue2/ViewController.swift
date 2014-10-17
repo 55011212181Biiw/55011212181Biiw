1dd224fc5b8903fb6e06cbf4515cc716e2f7dc45
55011212088aungaing <iMac@msu>
2014-10-17 14:19:47 +0700

    Segue

---
+++ Segue2/Segue2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Segue2
+//
+//  Created by Student on 10/17/2557 BE.
+//  Copyright (c) 2557 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var colorLabel: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
---
+++ Segue2/Segue2/color2FileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  color2FileViewController.swift
+//  Segue2
+//
+//  Created by Student on 10/17/2557 BE.
+//  Copyright (c) 2557 Student. All rights reserved.
+//
+
+import UIKit
+
+class color2FileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
