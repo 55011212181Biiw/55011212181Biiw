fcf8cde19a240f54668a86bd39eec5d9f748bee3
55011212088aungaing <iMac@msu>
2014-12-17 12:14:21 +0700

    Final

---
+++ Final_55011212181/Exam1_55011212181/Exam1_55011212181/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Exam1_55011212181
+//
+//  Created by Student on 12/17/2557 BE.
+//  Copyright (c) 2557 Student. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class ViewController: UIViewController{
+
+    var items = [NSManagedObject]()
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    @IBAction func add(sender: AnyObject) {
+   
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
+
